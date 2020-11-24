62aa6fd6bd54a8134f96bc69115bb9393d98242c
Chandler Craig <hellochandlercraig@gmail.com>
2020-11-24 00:26:20 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,9 +15,5 @@
 def speak_to_grandma(greeting)
   if greeting == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif "#{greeting}".upcase
-    return "NO, NOT SINCE 1938!"
-  else
-    return "HUH?! SPEAK UP, SONNY!"
   end
 end
